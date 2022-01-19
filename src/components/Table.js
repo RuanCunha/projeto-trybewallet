deb4ccd32a11b15e167af29b415ab88eee295c7c
RuanCunha <ruanvcunha@hotmail.com>
2022-01-18 21:48:31 -0300

    Requisito 7 finalizado

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,10 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <table>
         <tr>
@@ -15,9 +18,31 @@ class Table extends Component {
           <th>Moeda de conversão</th>
           <th>Editar/Excluir</th>
         </tr>
+        { expenses.map((e) => (
+          <tr key={ e.id }>
+            <td>{e.description}</td>
+            <td>{e.tag}</td>
+            <td>{e.method}</td>
+            <td>{e.value}</td>
+            <td>{(e.exchangeRates[e.currency].name).split('/', 1)}</td>
+            <td>{Number(e.exchangeRates[e.currency].ask).toFixed(2)}</td>
+            <td>
+              {(Number(e.exchangeRates[e.currency].ask) * Number(e.value)).toFixed(2)}
+            </td>
+            <td>Real</td>
+          </tr>
+        )) }
       </table>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps, null)(Table);
